9d957b8545bc421e11558a7d00b1384c457a5d82
qb-pranav <pranavt@qburst.com>
2024-09-24 15:48:11 +0530

    Added custom controller findOne for Product to fetch product details based on pid

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -4,4 +4,22 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::product.product');
+export default factories.createCoreController('api::product.product', ({ strapi }) => ({
+    async findOne(ctx) {
+        const { id: pid } = ctx.params; // Get PID from the URL
+    
+        // Call the custom service method to get the product by PID
+        const product = await strapi.service('api::product.product').findByPid(pid, ctx.query);
+        // Call pdp page configs
+        const pdpPage = await strapi.service('api::pdp-page.pdp-page').find();
+    
+        if (!product) {
+          return ctx.notFound('Product not found');
+        }
+    
+        return {
+            ...product,
+            ...pdpPage
+        };
+      },
+  }));
--- src/api/product/services/product.ts
+++ src/api/product/services/product.ts
@@ -4,4 +4,25 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::product.product');
+export default factories.createCoreService('api::product.product', ({ strapi }) => ({
+    // Custom service to find product by PID and populate PLP content
+    async findByPid(pid, queryParams) {
+        // Extract query parameters
+        const { locale, populate } = queryParams;
+        // Build the query object dynamically based on the params
+        const query = {
+          where: {
+            pid,
+            ...(locale && { locale }), // Conditionally add locale if it exists
+          },
+          populate: {
+            ...populate,
+          },
+        };
+        // Execute the query with the constructed query object
+        const product = await strapi.db.query('api::product.product').findOne(query);
+    
+        // Return the product or null if not found
+        return product;
+      },
+  }));
