5e1901d1cbc9e395fcbe1ae12dd1ff1a28b0121f
qb-pranav <pranavt@qburst.com>
2024-12-02 13:08:04 +0530

    Suggestion response structure update

--- src/api/suggestion/controllers/suggestion.ts
+++ src/api/suggestion/controllers/suggestion.ts
@@ -4,6 +4,7 @@
 
 import { factories } from '@strapi/strapi'
 import { enrichWithWishlist } from '../../product/services/wishlist-helper';
+import product from '../../product/controllers/product';
 
 export default factories.createCoreController('api::suggestion.suggestion', ({ strapi }) => ({
     async findOne(ctx) {
@@ -11,7 +12,8 @@ export default factories.createCoreController('api::suggestion.suggestion', ({ s
   
       const userId = ctx.state.userId;
       const brandId = ctx.request.header['brand-id'];
-      suggestions.data.attributes.products.data = await enrichWithWishlist(suggestions.data.attributes.products.data, userId, brandId);
+      const productData = suggestions.data.attributes.products.data.map(product => product.attributes);
+      suggestions.data.attributes.products.data = await enrichWithWishlist(productData, userId, brandId);
       return suggestions;
     },
 }));
